91452a8887f6e34391eed8468a87bac6d6756d88
Rocco Maniscalco <roccomaniscalco2001@gmail.com>
2022-04-06 17:18:55 -0400

    animate gameTile when letter is typed

--- client/src/components/GameTile.jsx
+++ client/src/components/GameTile.jsx
@@ -3,11 +3,17 @@ import { string } from "prop-types"
 import { letterEvaluation } from "../constants/propTypes"
 import { evals } from "../constants/qwerty"
 
-// execute when evaluation is revealed
-const flipIn = keyframes({
+// plays when evaluation is revealed
+const flip = keyframes({
   "0%": { transform: "rotateX(0)" },
   "50%": { transform: "rotateX(-90deg)" },
   "100%": { transform: "rotateX(0)" },
+})
+
+// plays when letter is typed
+const pop = keyframes({
+  "0%": { transform: "scale(0.8)", opacity: 0 },
+  "40%": { transform: "scale(1.1)", opacity: 1 },
 })
 
 const useStyles = createStyles((theme, { hasLetter }) => ({
@@ -22,11 +28,13 @@ const useStyles = createStyles((theme, { hasLetter }) => ({
     borderWidth: "0px",
     borderStyle: "solid",
     borderColor: hasLetter ? theme.colors.dark[3] : theme.colors.dark[4],
-    
-    // execute color and border changes halfway into flipIn animation
+
+    animation: hasLetter ? `${pop} 100ms ease-in-out` : undefined,
+
+    // change color and border halfway into flipIn animation
     transitionProperty: "border-width, background-color",
     transitionDuration: "0ms",
-    transitionDelay: "250ms"
+    transitionDelay: "250ms",
   },
 
   [evals.TBD]: {
@@ -35,15 +43,15 @@ const useStyles = createStyles((theme, { hasLetter }) => ({
   },
   [evals.UNUSED]: {
     backgroundColor: theme.colors.dark[6],
-    animation: `${flipIn} 500ms ease-in-out`,
+    animation: `${flip} 500ms ease-in-out`,
   },
   [evals.USED]: {
     backgroundColor: theme.fn.darken(theme.colors.yellow[6], 0.3),
-    animation: `${flipIn} 500ms ease-in-out`,
+    animation: `${flip} 500ms ease-in-out`,
   },
   [evals.CORRECT]: {
     backgroundColor: theme.fn.darken(theme.colors.green[6], 0.3),
-    animation: `${flipIn} 500ms ease-in-out`,
+    animation: `${flip} 500ms ease-in-out`,
   },
 }))
 

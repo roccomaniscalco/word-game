10bbd88bcd377d748fbd017f3379632ecdadcb4e
Rocco Maniscalco <roccomaniscalco2001@gmail.com>
2022-04-06 17:00:33 -0400

    animate gameTile when evaluation is revealed

--- client/src/components/GameTile.jsx
+++ client/src/components/GameTile.jsx
@@ -1,26 +1,50 @@
-import { Card, createStyles, Title } from "@mantine/core"
+import { Card, createStyles, keyframes, Title } from "@mantine/core"
 import { string } from "prop-types"
 import { letterEvaluation } from "../constants/propTypes"
 import { evals } from "../constants/qwerty"
+
+// execute when evaluation is revealed
+const flipIn = keyframes({
+  "0%": { transform: "rotateX(0)" },
+  "50%": { transform: "rotateX(-90deg)" },
+  "100%": { transform: "rotateX(0)" },
+})
 
 const useStyles = createStyles((theme, { hasLetter }) => ({
   tile: {
     color: theme.white,
     textTransform: "uppercase",
+
     display: "flex",
     alignItems: "center",
     justifyContent: "center",
+
+    borderWidth: "0px",
+    borderStyle: "solid",
+    borderColor: hasLetter ? theme.colors.dark[3] : theme.colors.dark[4],
+    
+    // execute color and border changes halfway into flipIn animation
+    transitionProperty: "border-width, background-color",
+    transitionDuration: "0ms",
+    transitionDelay: "250ms"
   },
 
   [evals.TBD]: {
     backgroundColor: theme.colors.dark[9],
     borderWidth: "2px",
-    borderStyle: "solid",
-    borderColor: hasLetter ? theme.colors.dark[2] : theme.colors.dark[4],
   },
-  [evals.UNUSED]: { backgroundColor: theme.colors.dark[6] },
-  [evals.USED]: { backgroundColor: theme.fn.darken(theme.colors.yellow[6], 0.3) },
-  [evals.CORRECT]: { backgroundColor: theme.fn.darken(theme.colors.green[6], 0.3) },
+  [evals.UNUSED]: {
+    backgroundColor: theme.colors.dark[6],
+    animation: `${flipIn} 500ms ease-in-out`,
+  },
+  [evals.USED]: {
+    backgroundColor: theme.fn.darken(theme.colors.yellow[6], 0.3),
+    animation: `${flipIn} 500ms ease-in-out`,
+  },
+  [evals.CORRECT]: {
+    backgroundColor: theme.fn.darken(theme.colors.green[6], 0.3),
+    animation: `${flipIn} 500ms ease-in-out`,
+  },
 }))
 
 const GameTile = ({ letter, evaluation }) => {

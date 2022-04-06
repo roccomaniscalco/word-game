5fbaabe3276dd604fbd428c8369acdcc1801751b
Rocco Maniscalco <roccomaniscalco2001@gmail.com>
2022-04-06 17:42:09 -0400

    refactor Keyboard to not use KeyboardRow

--- client/src/components/Keyboard.jsx
+++ client/src/components/Keyboard.jsx
@@ -1,22 +1,10 @@
-import { Grid, Stack, Title } from "@mantine/core"
+import { Grid, Title } from "@mantine/core"
 import { useElementSize } from "@mantine/hooks"
-import { func, node } from "prop-types"
+import { func } from "prop-types"
 import { Backspace as BackspaceIcon } from "tabler-icons-react"
 import { gameBoardRow, keys } from "../constants/propTypes"
 import Key from "./Key"
 import KeyEnter from "./KeyEnter"
-
-const KeyboardRow = ({ children }) => {
-  return (
-    <Grid columns={20} gutter={4} justify="center" sx={{ height: "100%" }}>
-      {children}
-    </Grid>
-  )
-}
-
-KeyboardRow.propTypes = {
-  children: node,
-}
 
 const Keyboard = ({ keys, onClick, currentRow }) => {
   const { ref, width } = useElementSize()
@@ -27,46 +15,47 @@ const Keyboard = ({ keys, onClick, currentRow }) => {
   const thirdRow = keys.slice(19, 26)
 
   return (
-    // preserve aspect ratio
-    <Stack spacing={4} ref={ref} sx={{ height: width / 2.8 }}>
-      <KeyboardRow>
-        {firstRow.map(({ letter, evaluation }) => (
-          <Grid.Col key={letter} span={2}>
-            <Key onClick={onClick} code={letter} evaluation={evaluation}>
-              <Title order={4}>{letter}</Title>
-            </Key>
-          </Grid.Col>
-        ))}
-      </KeyboardRow>
-
-      <KeyboardRow>
-        {secondRow.map(({ letter, evaluation }) => (
-          <Grid.Col key={letter} span={2}>
-            <Key onClick={onClick} code={letter} evaluation={evaluation}>
-              <Title order={4}>{letter}</Title>
-            </Key>
-          </Grid.Col>
-        ))}
-      </KeyboardRow>
-
-      <KeyboardRow>
-        <Grid.Col span={3}>
-          <KeyEnter onClick={onClick} currentRow={currentRow} />
-        </Grid.Col>
-        {thirdRow.map(({ letter, evaluation }) => (
-          <Grid.Col key={letter} span={2}>
-            <Key onClick={onClick} code={letter} evaluation={evaluation}>
-              <Title order={4}>{letter}</Title>
-            </Key>
-          </Grid.Col>
-        ))}
-        <Grid.Col span={3}>
-          <Key onClick={onClick} code="backspace">
-            <BackspaceIcon />
+    <Grid
+      columns={20}
+      gutter={4}
+      justify="center"
+      sx={{ height: width / 2.8 }} // preserve aspect ratio
+      ref={ref}
+    >
+      {firstRow.map(({ letter, evaluation }) => (
+        <Grid.Col key={letter} span={2}>
+          <Key onClick={onClick} code={letter} evaluation={evaluation}>
+            <Title order={4}>{letter}</Title>
           </Key>
         </Grid.Col>
-      </KeyboardRow>
-    </Stack>
+      ))}
+
+      {secondRow.map(({ letter, evaluation }) => (
+        <Grid.Col key={letter} span={2}>
+          <Key onClick={onClick} code={letter} evaluation={evaluation}>
+            <Title order={4}>{letter}</Title>
+          </Key>
+        </Grid.Col>
+      ))}
+
+      <Grid.Col span={3}>
+        <KeyEnter onClick={onClick} currentRow={currentRow} />
+      </Grid.Col>
+
+      {thirdRow.map(({ letter, evaluation }) => (
+        <Grid.Col key={letter} span={2}>
+          <Key onClick={onClick} code={letter} evaluation={evaluation}>
+            <Title order={4}>{letter}</Title>
+          </Key>
+        </Grid.Col>
+      ))}
+
+      <Grid.Col span={3}>
+        <Key onClick={onClick} code="backspace">
+          <BackspaceIcon />
+        </Key>
+      </Grid.Col>
+    </Grid>
   )
 }
 

fbdde909609a9ba201c3ca0e66375f3355acb0e8
Rocco Maniscalco <roccomaniscalco2001@gmail.com>
2022-04-16 05:47:39 -0400

    refactor Key styles

--- client/src/components/Key.jsx
+++ client/src/components/Key.jsx
@@ -1,83 +1,60 @@
-import { Button, createStyles, Title } from "@mantine/core"
+import { Button, createStyles, keyframes, Title } from "@mantine/core"
 import { elementType, func, string } from "prop-types"
 import { memo } from "react"
 import { letterEvaluation } from "../constants/propTypes"
 import { evals } from "../constants/qwerty"
+import useEvalColor from "../styles/useEvalColor"
 
-const useStyles = createStyles((theme) => ({
-  letterContainer: {
-    color: theme.colorScheme === "dark" ? theme.white : theme.black,
+const useStyles = createStyles((theme, { evaluation }) => {
+  const evalColor = useEvalColor()
 
-    backgroundColor:
-      theme.colorScheme === "dark" ? theme.colors.dark[4] : "#e9ecef",
-    "&:hover": {
-      backgroundColor:
-        theme.colorScheme === "dark" ? theme.colors.dark[4] : "#e9ecef",
+  // fill after row is revealed
+  const fill = keyframes({
+    "100%": {
+      backgroundColor: evalColor[evaluation],
     },
+  })
 
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
+  return {
+    button: {
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      width: "100%",
+      height: "100%",
+      padding: 0,
 
-    width: "100%",
-    height: "100%",
-    padding: 0,
-
-    // delay color change until after gameTiles are flipped
-    transitionProperty: "background-color",
-    transitionDuration: "0ms",
-    transitionDelay: "1250ms",
-  },
-
-  [evals.UNUSED]: {
-    backgroundColor:
-      theme.colorScheme === "dark"
-        ? theme.colors.dark[6]
-        : theme.colors.dark[0],
-    "&:hover": {
+      color: theme.colorScheme === "dark" ? theme.white : theme.black,
       backgroundColor:
         theme.colorScheme === "dark"
-          ? theme.colors.dark[6]
-          : theme.colors.dark[0],
+          ? theme.colors.dark[4]
+          : theme.colors.gray[2],
+      "&:hover": {
+        backgroundColor:
+          theme.colorScheme === "dark"
+            ? theme.colors.dark[4]
+            : theme.colors.gray[2],
+      },
     },
-  },
 
-  [evals.USED]: {
-    backgroundColor:
-      theme.colorScheme === "dark"
-        ? theme.fn.darken(theme.colors.yellow[6], 0.3)
-        : theme.colors.yellow[6],
-    "&:hover": {
-      backgroundColor:
-        theme.colorScheme === "dark"
-          ? theme.fn.darken(theme.colors.yellow[6], 0.3)
-          : theme.colors.yellow[6],
+    fill: {
+      animation: `${fill} 0ms forwards ${1250}ms`,
     },
-  },
-
-  [evals.CORRECT]: {
-    backgroundColor:
-      theme.colorScheme === "dark"
-        ? theme.fn.darken(theme.colors.green[6], 0.3)
-        : theme.colors.green[6],
-    "&:hover": {
-      backgroundColor:
-        theme.colorScheme === "dark"
-          ? theme.fn.darken(theme.colors.green[6], 0.3)
-          : theme.colors.green[6],
-    },
-  },
-}))
+  }
+})
 
 const Key = ({ onClick, code, Icon, evaluation, ...props }) => {
-  const { classes } = useStyles()
+  const { classes, cx } = useStyles({ evaluation })
 
   return (
     <Button
       value={code}
       onClick={onClick}
-      className={`${classes.letterContainer} ${classes[evaluation]}`}
       uppercase
+      className={cx(
+        classes.button,
+        evaluation !== evals.TBD && evaluation !== undefined && classes.fill
+      )}
       {...props}
     >
       {Icon ? <Icon /> : <Title order={4}>{code}</Title>}

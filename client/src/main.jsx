068961b568be78303d8f15b3542eb4b0cb236e29
Rocco Maniscalco <roccomaniscalco2001@gmail.com>
2022-04-18 18:40:12 -0400

    move ThemeProvider to /styles

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 import App from "./App"
 import { StatsProvider } from "./contexts/StatsContext"
-import ThemeProvider from "./contexts/ThemeProvider"
+import ThemeProvider from "./styles/ThemeProvider"
 
 const root = createRoot(document.getElementById("root"))
 
